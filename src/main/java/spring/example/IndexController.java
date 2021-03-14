eab3abd715b5992cd2f826d9d28d780a3344068d
nut.sontikoon <nut.sontikoon@gmail.com>
2021-03-14 22:19:28 +0700

    first commit

---
+++ src/main/java/spring/example/IndexController.java
@@ -0,0 +1,36 @@
+/** \file
+ * 
+ * Mar 21, 2018
+ *
+ * Copyright Ian Kaplan 2018
+ *
+ * @author Ian Kaplan, www.bearcave.com, iank@bearcave.com
+ */
+package spring.example;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    
+    /**
+     * Controller function for the index HTML page.
+     */
+    @RequestMapping("/")
+    public String index(Model model) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM d yyyy : HH:mm:ss:SSS z");
+        Date now = new Date();
+        String dateStr = dateFormat.format( now );
+        model.addAttribute("time", dateStr);
+        return "index";
+    }
+    @RequestMapping("/welcome")
+    public String welcome(Model model) {
+        return "welcome";
+    }
+}
